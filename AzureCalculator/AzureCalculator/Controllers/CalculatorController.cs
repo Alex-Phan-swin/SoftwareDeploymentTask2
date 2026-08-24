ea1df137601f9aca726b5ac6f67b0d15434d48ff
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-08-24 15:28:13 +1000

    Controller class

---
+++ AzureCalculator/AzureCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AzureCalculator.Models;
+
+namespace AzureCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model) 
+        {
+            switch (model.Operation)
+            {
+                case "+":
+                    model.Result = model.Number1 + model.Number2;
+                    break;
+
+                case "-":
+                    model.Result = model.Number1 - model.Number2;
+                    break;
+
+                case "*":
+                    model.Result = model.Number1 * model.Number2;
+                    break;
+
+                case "/":
+                    if (model.Number2 == 0)
+                    {
+                        model.ErrorMessage = "Cant divide by zero";
+                    }
+                    else 
+                    {
+                        model.Result = model.Number1 / model.Number2;
+                    }
+                    break;
+
+                default:
+                    model.ErrorMessage = "Retry operation";
+                    break;
+                        
+            }
+
+            return View(model);
+        }
+    }
+}
