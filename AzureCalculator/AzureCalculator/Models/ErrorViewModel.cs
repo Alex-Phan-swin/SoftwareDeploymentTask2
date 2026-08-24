e73dae1c6d08d07c8af2294ba3043325839a6799
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-08-24 15:14:56 +1000

    Model layer

---
+++ AzureCalculator/AzureCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
