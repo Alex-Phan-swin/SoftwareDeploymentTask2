075437ee0be947e0afafaf3d4f1f169cdbd222bb
Alex-Phan-swin <105334704@student.swin.edu.au>
2026-08-24 16:48:35 +1000

    Added razor directive

--- AzureCalculator/AzureCalculator/Program.cs
+++ AzureCalculator/AzureCalculator/Program.cs
@@ -28,7 +28,7 @@ namespace AzureCalculator
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Calculator}/{action=Index}/{id?}");
 
             app.Run();
         }
